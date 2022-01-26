0e80ab62c9a264b3595a0ea42b07715da86037c0
DeysiLopes <deysilinconl@gmail.com>
2022-01-26 17:21:56 -0300

    Adiciona animação Flip

--- src/components/CardFontBack/index.js
+++ src/components/CardFontBack/index.js
@@ -1,10 +1,16 @@
 import cardGame from '/src/components/cardGame/index'
+import './style.css'
 
 function cardFrontBack(){
     return /*html*/ `
         <article class="card-front-back">
-            ${cardGame("")}
-            ${cardGame("charmander", "Charmander")}
+            <div class="card -front">
+                ${cardGame()}
+            </div>
+            <div class="card -back">
+                ${cardGame("charmander", "Charmander")}
+            </div>
+           
             
         </article>
     `
