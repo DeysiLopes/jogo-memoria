0900df8d3575afc2eafab91aad359cc119ce8afc
DeysiLopes <deysilinconl@gmail.com>
2022-01-28 13:41:14 -0300

    Cria component vsPlayer sem CSS

---
+++ src/components/vsPlayer/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+
+function vsPlayer (content = 'vs'){
+    return /*html*/`
+        <span class="vs-player">
+            ${content}
+        </span>
+    ` 
+}
+export default vsPlayer;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,10 +1,12 @@
 import playerName from "../../components/PlayerName";
+import vsPlayer from "../../components/vsPlayer";
 import './style.css'
 
 function scoreBoard (){
     return /*html*/ `
         <header class="score-board">
-        ${playerName('Player 1')}    
+        ${playerName('Player 1')} 
+        ${vsPlayer()}   
         ${playerName('Player 2')}
         </header>
     `
