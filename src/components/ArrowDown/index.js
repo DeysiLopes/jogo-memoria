8567dfcbe1077a3f441c6076d23baffe39bc02cf
DeysiLopes <deysilinconl@gmail.com>
2022-01-28 19:06:40 -0300

    Adiciona CSS no ArrowDown

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,6 +1,12 @@
-function arrowDown(){
+import './style.css'
+
+function arrowDown(currentPlayer = 1){
     return /*html*/ `
-        <img src="/images/ArrowDown.svg" alt="ArrowDown">
+        <img 
+        data-currentPlayer = "${currentPlayer}"
+        class="arrow-down" 
+        src="/images/ArrowDown.svg" 
+        alt="ArrowDown">
     `
 }
 export default arrowDown;
