f98e93fe2f47ed5278f665a03ff191cf07d5b3b3
DeysiLopes <deysilinconl@gmail.com>
2022-01-28 22:41:35 -0300

    Cria regra de negócio do jogo

--- src/objects/boardGames/index.js
+++ src/objects/boardGames/index.js
@@ -3,12 +3,36 @@ import cardFrontBack from '../../components/CardFontBack';
 import cards from "./Data"
 
 function boardGame(amountCards){
+    const flipAndHideCards= ($cardsActive) => {
+        $cardsActive.forEach((card) => card.classList.remove('-active'));
+    }
+    const swapPlayer = ()=> {
+        const $ArrowDown = document.querySelector('.arrow-down');
+        const currentPlayer = $ArrowDown.getAttribute('data-currentPlayer');
+        $ArrowDown.setAttribute('data-currentPlayer', currentPlayer == 2 ? 1 : 2)
+    }
+
+    window.BoardGame = {};
+    window.BoardGame.handleClick = () => {
+        const $BoardGame = document.querySelector('.board-game');
+        
+        const $cardsActive = $BoardGame.querySelectorAll('.card-front-back.-active')
+        //console.log($cardsActive.length)
+       if ($cardsActive.length === 2){
+           setTimeout(() => {
+                
+               flipAndHideCards($cardsActive);
+               swapPlayer();
+               
+           }, 1500);
+       }
+    }
    
     const htmlCardsList = cards.map((card) => cardFrontBack(card.icon, card.altIcon))
     const $htmlCards = htmlCardsList.join('');
 
     return /*html*/`
-        <section class="board-game">
+        <section class="board-game" onclick="BoardGame.handleClick()">
             ${$htmlCards}
         </section>
     `;
